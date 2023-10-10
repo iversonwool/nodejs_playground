2fc9c9c066e289e54356f335a2f48aae3e8bbc8d
Lee_Klaus <iversonwoool@gmail.com>
2023-10-10 11:40:59 +0800

    模拟请求

--- nodePlayground/index.js
+++ nodePlayground/index.js
@@ -6,9 +6,10 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-  console.log(__dirname)
-  console.log(__filename)
+app.get('/text', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // console.log(__dirname)
+  // console.log(__filename)
   res.send('你好，中国！')
 })
 
