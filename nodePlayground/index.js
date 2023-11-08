45c86e4cf37a9f6c6c007c9b7d1db96281413622
Lee_Klaus <iversonwoool@gmail.com>
2023-11-08 14:41:46 +0800

    send file

--- nodePlayground/index.js
+++ nodePlayground/index.js
@@ -7,10 +7,25 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, 'public')))
 
 app.get('/text', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Origin', '*');
   // console.log(__dirname)
   // console.log(__filename)
-  res.send('你好，中国！')
+  // res.send('你好，中国！')
+
+  res.setHeader('content-type', 'text/html; charset=utf-8');
+  // res.send(`
+  //   <!DOCTYPE html>
+  //   <html lang="en>
+  //     <head>
+  //       <title>h</title>
+  //     </head>
+  //     <body>
+  //       <div>hello !</div>
+  //     </body>
+  //   </html>
+  // `)
+
+  res.sendFile(path.resolve(__dirname, './public/index.html'))
 })
 
 app.listen(3333, function () {
